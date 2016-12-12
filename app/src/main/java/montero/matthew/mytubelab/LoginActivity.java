177dde3f4b113806276cea6948b2983b9960c88b
mattmontero <matthewrmontero@gmail.com>
2016-12-12 12:51:01 -0800

    google sign in

--- app/src/main/java/montero/matthew/mytubelab/LoginActivity.java
+++ app/src/main/java/montero/matthew/mytubelab/LoginActivity.java
@@ -1,13 +1,139 @@
 package montero.matthew.mytubelab;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.OptionalPendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 
-public class LoginActivity extends AppCompatActivity {
+
+public class LoginActivity extends AppCompatActivity
+        implements GoogleApiClient.OnConnectionFailedListener,
+        View.OnClickListener {
+
+    private GoogleApiClient mGoogleApiClient;
+    private static final int RC_SIGN_IN = 9001;
+    private TextView mStatusTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //View
+        mStatusTextView = (TextView) findViewById(R.id.status);
+
+        // Registering signIn buttons OnClickListener to sign in.
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
+        findViewById(R.id.sign_out_button).setOnClickListener(this);
+        findViewById(R.id.continueAs_button).setOnClickListener(this);
+
+        // Configure sign-in to request the user's ID, email address, and basic profile.
+        // ID and basic provile are included in DEFAULT_SIGN_IN
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        // Build a GoogleApiClient with access to the Google Sign-In API and the
+        // options specified by gso.
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this,this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        updateSignInUI(false);
+    }
+
+    private void signOut() {
+        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
+                new ResultCallback<Status>(){
+                    @Override
+                    public void onResult(Status status) {
+                        //update UI
+                        updateSignInUI(false);
+                    }
+                }
+        );
+    }
+
+    private void signIn(){
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d("LoginActivity", "handleSignInResult:" + result.isSuccess());
+        if(result.isSuccess()) {
+            //Signed in successully.
+            GoogleSignInAccount acct = result.getSignInAccount();
+            //display username after signin.
+            mStatusTextView.setText(getString(R.string.signed_in_as, acct.getDisplayName()));
+            //update UI
+            updateSignInUI(true);
+        } else {
+            //Signed out, show unauthenticated UI
+            //update UI
+            updateSignInUI(false);
+        }
+    }
+
+    private void updateSignInUI(boolean signedIn) {
+        if(signedIn){
+            findViewById(R.id.sign_in_button).setVisibility(View.GONE);
+            findViewById(R.id.sign_out_and_continue).setVisibility(View.VISIBLE);
+        } else {
+            mStatusTextView.setText(R.string.signed_out);
+            findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
+            findViewById(R.id.sign_out_and_continue).setVisibility(View.GONE);
+        }
+
+    }
+    private void bringToHomepage()
+    {
+        //load new activity for video search
+        //Intent intent = new Intent(this, MainActivity.class);
+        //startActivity(intent);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if(requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.sign_in_button:
+                signIn();
+                break;
+            case R.id.sign_out_button:
+                signOut();
+                break;
+            case R.id.continueAs_button:
+                bringToHomepage();
+                break;
+        }
     }
 }
